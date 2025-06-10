1d8d0aa55eeaa8002ba12d030568ca568e53defe
yabtsegaAschalew <yabtsegaaschalew1@gmail.com>
2025-06-10 23:45:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -38,7 +38,47 @@ class Graph
         }
     }
 
-    void dijkstraShortestPath(int src, int dest);
+    // Dijkstra's algorithm to find the shortest path from a source vertex
+    void dijkstraShortestPath(int src, int dest)
+    {
+      if (src <0 || src >= numVertices || dest < 0 || dest >= numVertices){
+        cerr<<"Error: Source or destination vertex out of bounds. "<< endl;
+        return;
+      }
+      // dist[i] will hold the shortest distance from src to i
+      vector<int> dist(numVertices, INF);
+
+      // parent[i] will store the predecessor of i in the shortest path from src
+      vector<int> parent(numVertices, -1);
+
+      // Priority queue to store vertices that are being preprocessed.
+      // We use pair<int, int> where first element is distance and second is vertex label.
+      // Greater makes it a min-priority queue (smallest distance on top).
+      priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+      // Distance from source to itself is always 0.
+      dist[src] = 0;
+      pq.push({0, src}); // {distance, vertex}
+
+      while (!pq.empty()){
+        int u = pq.top().second; // Vertex with the smallest distance
+        int d_u = pq.top().first; // Distance to u
+        pq.pop();
+
+        // If we found a shorter path already, skip
+        // This happens id a vertex is pushed multiple times into PQ with different distances 
+
+        if(d_u > dist[u]){
+          continue;
+        }
+
+        // If we reached the destination, we can potentially stop early
+        // For finding all shortest paths from src, remove this check.
+        if(u == dest && dist[u] != INF ){
+          // break; // Optimization: stop if destination is reached
+        }
+      }
+    }
 
     void printGraph() const;
 };
