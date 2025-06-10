8118e6844a6b66ab20c6e66105faeaaba0611c39
meron21-co <meronalemayehu2486@gmail.com>
2025-06-10 23:23:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<list>
 using namespace std;
 
 // Define infinity as a large enough value
@@ -10,7 +11,38 @@ struct Edge
 {
   int to;
   int weight;
-}
+};
+
+// Graph class
+class Graph
+{
+  public:
+    int numVertices;
+    // Adjacency list: vector of lists, where each list contains pairs of (destination_vertex, weight)
+    vector<list<Edge>> adj;
+
+    Graph(int V) : numVertices(V), adj(V);
+
+    // Add an edge to a directed graph
+    void addEdge(int u, int v, int weight)
+    {
+      if(u >= 0 && u < numVertices && v >= 0 && v < numVertices)
+        {
+            adj[u].push_back({v,weight});
+           // If it's an undirected graph, add the reverse edge too:
+          // adj[v].push_back({u,weight});
+        }
+      else 
+        {
+          cerr<<"Error: Vertex out of bounds."<<endl;
+        }
+    }
+
+    void dijkstraShortestPath(int src, int dest);
+
+    void printGraph() const;
+};
+
 int main(){
 
   return 0;
