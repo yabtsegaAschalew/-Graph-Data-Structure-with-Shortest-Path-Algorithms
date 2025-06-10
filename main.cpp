d8476b6d3c5bfa5e35397928a43078e2676584e4
hewan-HEAL <hewanabebe24@gmail.com>
2025-06-10 15:54:45 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 #include<vector>
 using namespace std;
 
+// Define infinity as a large enough value
+const int INF = numeric_limits<int>::max(); 
+
+// Structure to represent a weighted edge in the graph
+struct Edge
+{
+  int to;
+  int weight;
+}
 int main(){
 
   return 0;
