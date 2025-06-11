0acfac0bb160f41154f8d914f095f7aa701bed59
Dibora-Asrat <diboraasrat.45@gmail.com>
2025-06-11 22:33:36 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -139,8 +139,26 @@ class Graph
     }
 };
 
-int main(){
+int main()
+{
+  // Create a graph with 6 vertices
+  Graph g(6);
 
+  // Add edges: u, v, weight
+  g.addEdge(0, 1, 5);
+  g.addEdge(0, 2, 1);
+  g.addEdge(1, 2, 2);
+  g.addEdge(1, 3, 1);
+  g.addEdge(2, 3, 4);
+  g.addEdge(2, 4, 8);
+  g.addEdge(3, 4, 3);
+  g.addEdge(3, 5, 6);
+  g.addEdge(4, 5, 2);
+
+  cout << "-------------------- Graph Details --------------------"<<endl;
+  g.printGraph();
+  cout << "-------------------------------------------------------"<<endl;
+  
   return 0;
 }
 
